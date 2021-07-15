8dd4a3593d5a4f1bb19b6f4621e168b8a0260e0d
Harsh SIngh <hi.harsh@protonmail.ch>
2021-07-15 16:39:55 -0400

    Firebase configuration for prod

--- components/FirebaseSDK.ts
+++ components/FirebaseSDK.ts
@@ -1,16 +1,14 @@
 import firebase from 'firebase'
-import config from './FirebaseSDK.config'
 
 const FirebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY || config.apiKey,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN || config.authDomain,
-  databaseURL: process.env.FIREBASE_DATABASE_URL || config.databaseURL,
-  projectId: process.env.FIREBASE_PROJECT_ID || config.projectId,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || config.storageBucket,
-  messagingSenderId:
-    process.env.FIREBASE_MESSAGING_SENDER_ID || config.messagingSenderId,
-  appId: process.env.FIREBASE_APP_ID || config.appId,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID || config.measurementId,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 }
 
 !firebase.apps.length ? firebase.initializeApp(FirebaseConfig) : firebase.app()
