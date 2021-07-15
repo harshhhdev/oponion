66f84710b1b78c462cd9f38159f7d92cb429ed17
Harsh SIngh <hi.harsh@protonmail.ch>
2021-07-15 13:54:36 -0400

    Added banner

--- components/Head.tsx
+++ components/Head.tsx
@@ -26,6 +26,7 @@ const Header: React.FC<{ title: string }> = ({ title }) => {
         rel="icon"
         href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📈</text></svg>"
       />
+      <meta property="og:image" content="https://github.com/harshhhdev/" />
     </Head>
   )
 }
